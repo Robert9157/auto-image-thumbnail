81bc4c8fb1d46930ef2c8d452ef813bce4ee4333
Robert9157 <robi@valkai.net>
2022-01-13 22:51:15 +0100

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-SIZE="192x192" # output image size
-QUALITY=50% # jpeg quality
-LIST=`ls *.jpg *.png *.jpeg` # list of image types to convert
+LIST=`ls *.jpg *.png *.jpeg`
 
 for i in $LIST;
-	do convert $i -resize $SIZE ./done/$i;convert -strip -interlace Plane -gaussian-blur 0.05 -quality $QUALITY done/$i done/$i;done
+	do convert $i -resize $1 ./$OUTPUTDIR/$i;convert -strip -interlace Plane -gaussian-blur 0.05 -quality $2 $3/$i $3/$i;done
